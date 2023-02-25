af9dd7d7c25055b90ef1a752d6f6a6eb68d7a8da
Orlandroid <lopez.orlando.gold9@gmail.com>
2023-02-24 20:48:33 -0600

    [WIP] Detalle

---
+++ app/src/main/java/com/example/androidbase/presentation/ui/character_detail/CharacterDetailFragment.kt
@@ -0,0 +1,19 @@
+package com.example.androidbase.presentation.ui.character_detail
+
+import com.example.androidbase.R
+import com.example.androidbase.databinding.FragmentCharacterDetailBinding
+import com.example.androidbase.presentation.base.BaseFragment
+
+
+class CharacterDetailFragment :
+    BaseFragment<FragmentCharacterDetailBinding>(R.layout.fragment_character_detail) {
+
+    override fun setUpUi() {
+
+    }
+
+    override fun observerViewModel() {
+        super.observerViewModel()
+    }
+
+}
--- domain/src/main/java/com/example/domain/entities/remote/PeopleResponseItem.kt
+++ domain/src/main/java/com/example/domain/entities/remote/PeopleResponseItem.kt
@@ -2,33 +2,33 @@ package com.example.domain.entities.remote
 
 data class PeopleResponseItem(
     val affiliations: List<String>,
-    //val apprentices: List<String>, // and String
-    //val armament: List<String>, // and String
+    val apprentices: List<String>,
+    val armament: List<String>,
     val born: Any,
     val bornLocation: String,
     val `class`: String,
     val creator: String,
-    //val cybernetics: String,  String and List<String>
+    val cybernetics: List<String>,
     val dateCreated: Int,
     val dateDestroyed: Int,
     val degree: String,
     val destroyedLocation: String,
     val died: Int,
     val diedLocation: String,
-    //val equipment: String, String and List<String>
-    //val era: List<String>, // And String
+    val equipment: List<String>,
+    val era: List<String>,
     val eyeColor: String,
     val formerAffiliations: List<String>,
     val gender: String,
     val hairColor: String,
     val height: Double,
-    //val homeworld: String, // and List<String>
+    val homeworld: List<String>,
     val id: Int,
     val image: String,
     val kajidic: String,
     val manufacturer: String,
     val mass: Double,
-    //val masters: List<String>, and String
+    val masters: List<String>,
     val model: String,
     val name: String,
     val platingColor: String,
